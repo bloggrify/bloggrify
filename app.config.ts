586d273c2ff6c861cb36bbbf207e2e471dfb2c18
hlassiege <hugo@malt.com>
2024-03-05 18:43:19 +0100

    we should not have default configuration for socials

--- app.config.ts
+++ app.config.ts
@@ -14,13 +14,13 @@ export default defineAppConfig({
         "lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
 
     socials: {
-        twitter: "https://twitter.com",
-        mastodon: "https://piaille.fr",
-        youtube: "https://youtube.com",
-        linkedin: "https://linkedin.com",
-        facebook: "https://facebook.com",
-        instagram: "https://instagram.com",
-        github: "https://github.com",
+        // twitter: "https://twitter.com",
+        // mastodon: "https://piaille.fr",
+        // youtube: "https://youtube.com",
+        // linkedin: "https://linkedin.com",
+        // facebook: "https://facebook.com",
+        // instagram: "https://instagram.com",
+        // github: "https://github.com",
     },
 
     newsletter: {
@@ -39,24 +39,24 @@ export default defineAppConfig({
 
     // if you have multiple authors, you can set them here
     authors: [
-        {
-            default: true,
-            username: "john-doe",
-            name: "John Doe",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
-            avatar: "/images/avatar.jpg",
-            socials: {
-                twitter: "https://twitter.com",
-                twitter_username: "username",
-                mastodon: "https://piaille.fr",
-                youtube: "https://youtube.com",
-                linkedin: "https://linkedin.com",
-                facebook: "https://facebook.com",
-                instagram: "https://instagram.com",
-                github: "https://github.com",
-            },
-        },
+        // {
+        //     default: true,
+        //     username: "john-doe",
+        //     name: "John Doe",
+        //     description:
+        //         "lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.",
+        //     avatar: "/images/avatar.jpg",
+        //     socials: {
+        //         twitter: "https://twitter.com",
+        //         twitter_username: "username",
+        //         mastodon: "https://piaille.fr",
+        //         youtube: "https://youtube.com",
+        //         linkedin: "https://linkedin.com",
+        //         facebook: "https://facebook.com",
+        //         instagram: "https://instagram.com",
+        //         github: "https://github.com",
+        //     },
+        // },
     ],
 
     menu: [
