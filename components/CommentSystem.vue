294ee08e59d53bb6d35855df182d7b2466ddeb68
hlassiege <hugo@malt.com>
2024-03-06 22:54:24 +0100

    Generic component for all theme to include the comment system, compatible with dark mode

---
+++ components/CommentSystem.vue
@@ -0,0 +1,32 @@
+<template>
+    <hyvor-talk-comments
+        v-if="isCommentsEnabled"
+        :website-id="config.comments.hyvor_talk.website_id"
+        :page-id="id"
+        class="dark:text-white"
+        :colors="isDark ? 'dark' : 'light'"
+    ></hyvor-talk-comments>
+</template>
+<script setup lang="ts">
+import { useDark } from "@vueuse/core";
+const props = defineProps<{
+    id: string | undefined;
+    nocomments: boolean | undefined;
+}>();
+
+const config = useAppConfig();
+const isDark = useDark();
+const isCommentsEnabled = config.comments.enabled && !props.nocomments;
+
+if (isCommentsEnabled) {
+    useHead({
+        script: [
+            {
+                async: true,
+                src: "https://talk.hyvor.com/embed/embed.js",
+                type: "module",
+            },
+        ],
+    });
+}
+</script>
