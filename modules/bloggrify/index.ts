a634eadf8405bfc0f6578a48a77ce40a3439b2c6
hlassiege <hugo@malt.com>
2024-05-20 15:36:52 +0200

    build: break the build if the content folder is missing

--- modules/bloggrify/index.ts
+++ modules/bloggrify/index.ts
@@ -1,9 +1,7 @@
 import {consola} from 'consola'
 import {colors} from 'consola/utils'
 import {defineNuxtModule} from '@nuxt/kit'
-const fse = require('fs-extra')
-
-import pkg from '../../package.json'
+import fse from 'fs-extra'
 
 export default defineNuxtModule({
     meta: {
@@ -12,13 +10,23 @@ export default defineNuxtModule({
     },
     setup (options, nuxt) {
 
-        // check presence of content folder
-        fse.pathExists('~/content', (err: any, exists: any) => {
-            consola.box(
-                colors.greenBright('Bloggrify') + '\n\n' +
-                        'Missing `content` folder, please add it to your project. Most of the time, this error occurs when you try to start Bloggrify without using an existing theme.  \n\n' +
-                        colors.blueBright('Bloggrify is not designed to work without any content. You should follow the documentation here https://bloggrify.com/introduction/installation .')
-            )
+        nuxt.hook('build:before', async () => {
+            fse.pathExists(nuxt.options.rootDir + '/content', (err: any, exists: any) => {
+                if (exists) return
+                consola.box(
+                    colors.greenBright('Bloggrify') + '\n\n' +
+                    'Missing `content` folder, please add it to your project. Most of the time, this error occurs when you try to start Bloggrify without using an existing theme.  \n\n' +
+                    colors.blueBright('Bloggrify is not designed to work without any content. You should follow the documentation here https://bloggrify.com/introduction/installation .')
+                )
+                throw _createError('Missing `content` folder.')
+            })
         })
+
     }
 })
+
+function _createError (message: string) {
+    const error = new Error(message)
+    try {error.stack = ''} catch { /* runtime not supports */ }
+    return error
+}
