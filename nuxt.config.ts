104da4267c2285f5e0d2caaea97f9824f65fd6bd
Hugo Lassiège <hugo@malt.com>
2024-11-23 08:52:16 +0900

    chore: nuxt is not yet ready for the new version of sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,66 +1,73 @@
 export default defineNuxtConfig({
-    routeRules: {
-        '/api/search': {
-            prerender: true
-        }
-    },
+  routeRules: {
+      '/api/search': {
+          prerender: true
+      }
+  },
 
-    devtools: { enabled: true },
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@nuxt/content',
-        '@nuxt/image',
-        '@stefanobartoletti/nuxt-social-share'
-    ],
-    image: {
-        format: ['webp'],
-    },
-    content: {
-        markdown: {
-            remarkPlugins: ['remark-reading-time', 'remark-math', 'remark-mermaidjs'],
-            rehypePlugins: ['rehype-katex'],
-        },
-        highlight: {
-            langs: [
-                'json',
-                'js',
-                'javascript',
-                'ts',
-                'typescript',
-                'html',
-                'css',
-                'vue',
-                'shell',
-                'bash',
-                'mdc',
-                'mermaid',
-                'md',
-                'yaml',
-                'python',
-                'c',
-                'csharp',
-                'cpp',
-                'sql',
-                'java',
-                'xml',
-                'scala',
-                'kotlin',
-            ],
-            theme: {
-                default: 'catppuccin-frappe',
-                // Theme used if `html.dark`
-                dark: 'github-dark',
-            },
-        },
-    },
-    nitro: {
-        prerender: {
-            routes: ['/sitemap.xml', '/rss.xml', '/robots.txt'],
-        },
-    },
-    vue: {
-        compilerOptions: {
-            isCustomElement: (tag) => ['hyvor-talk-comments'].includes(tag),
-        },
-    },
+  devtools: { enabled: true },
+
+  modules: [
+      '@nuxtjs/tailwindcss',
+      '@nuxt/content',
+      '@nuxt/image',
+      '@stefanobartoletti/nuxt-social-share'
+  ],
+
+  image: {
+      format: ['webp'],
+  },
+
+  content: {
+      markdown: {
+          remarkPlugins: ['remark-reading-time', 'remark-math', 'remark-mermaidjs'],
+          rehypePlugins: ['rehype-katex'],
+      },
+      highlight: {
+          langs: [
+              'json',
+              'js',
+              'javascript',
+              'ts',
+              'typescript',
+              'html',
+              'css',
+              'vue',
+              'shell',
+              'bash',
+              'mdc',
+              'mermaid',
+              'md',
+              'yaml',
+              'python',
+              'c',
+              'csharp',
+              'cpp',
+              'sql',
+              'java',
+              'xml',
+              'scala',
+              'kotlin',
+          ],
+          theme: {
+              default: 'catppuccin-frappe',
+              // Theme used if `html.dark`
+              dark: 'github-dark',
+          },
+      },
+  },
+
+  nitro: {
+      prerender: {
+          routes: ['/sitemap.xml', '/rss.xml', '/robots.txt'],
+      },
+  },
+
+  vue: {
+      compilerOptions: {
+          isCustomElement: (tag) => ['hyvor-talk-comments'].includes(tag),
+      },
+  },
+
+  compatibilityDate: '2024-11-23',
 })
