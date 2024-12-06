caa956d823ff9a68309eda20f18e1b3669ff6a9c
IO-Fire <96652894+IO-Fire@users.noreply.github.com>
2024-12-06 12:51:26 +0000

    feat: invalid layout

---
+++ layouts/invalid.vue
@@ -0,0 +1,19 @@
+<!-- This layout exists to throw an error if the intended layout does not exist -->
+<!-- Example <NuxtLayout :name="my-missing-layout" fallback='invalid' /> -->
+
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+// Throw an error instead of showing the layout
+throw createError({
+    name: 'Invalid layout',
+    fatal: false,
+    statusMessage: 'Invalid layout'
+})
+</script>
+
