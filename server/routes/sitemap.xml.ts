bad8e9f26885070216ba75df53ba85e58f9ef9bb
Hugo Lassiège <hugo@malt.com>
2024-12-07 20:12:33 +0900

    feat: add xsl to the sitemap and include cover if they exist

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,21 +1,29 @@
 import { serverQueryContent } from '#content/server'
 import { SitemapStream, streamToPromise } from 'sitemap'
+import {cleanDoubleSlashes, withoutLeadingSlash, withTrailingSlash} from 'ufo'
+
 export default defineEventHandler(async (event) => {
     const config = useAppConfig()
-    const url = config.url?.replace(/\/$/, '')
+    const url = withTrailingSlash(config.url)
 
     const docs = await serverQueryContent(event)
-        .where({ hidden: { $ne: true } })
+        .where({ hidden: { $ne: true }, draft: { $ne: true } })
         .find()
     const sitemap = new SitemapStream({
         hostname: url,
+        xslUrl: 'style.xsl',
     })
     for (const doc of docs) {
         sitemap.write({
-            url: url + doc._path,
-            lastmod: doc.date,
+            url: cleanDoubleSlashes(url + withoutLeadingSlash(doc._path)),
+            lastmod: doc.date ,
+            img: doc.cover ? [{
+                url: url + withoutLeadingSlash(doc.cover),
+                caption: doc.title || '',
+            }] : undefined
         })
     }
+
     sitemap.end()
     return streamToPromise(sitemap)
 })
